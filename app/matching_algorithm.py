69bc81a9a80ad7aa9042e662ed15208132153a21
chloehxf <246687@students.hertie-school.org>
2025-03-21 12:00:51 +0100

    Create matching_algorithm.py

---
+++ app/matching_algorithm.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import requests
+import datetime
+import WTforms
+
+# Function: Create and return random group name
+
+
+# Function: Calculate centroid location
+
+
+# Function: Calculate area of radial circle from centroid
+
+
+# Function: Return restaurants within radial circle
+
+
+# Function: Add weights to each item and calculate a composite score
+
