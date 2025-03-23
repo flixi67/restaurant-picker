7ec7a880919148c54c994acb2adb1c20db62da51
Felix Kube <F.Kube@students.hertie-school.org>
2025-03-23 16:22:04 +0100

    Finish frame

--- app/modules/01_data-pipeline-google-API.py
+++ app/modules/01_data-pipeline-google-API.py
@@ -1,12 +1,37 @@
 import requests
 import json
 import pandas as pd
+import sys
+import os
+sys.path.append(os.path.abspath("app/modules/"))
+from geocode import GoogleGeocodingAPI
+from shapely.geometry import MultiPoint
+from geopy.distance import geodesic
+
 
 # API endpoint
 url = "https://places.googleapis.com/v1/places:searchNearby"
 
-# Your API Key (Replace with your actual key)
+# Your API Key
 api_key = "AIzaSyAwNuqmopqXKbaH1VM-P5SgQRm4V4lQa0o"
+
+# Calculating centroid coordinates for API request
+geocoder = GoogleGeocodingAPI(api_key=api_key)
+
+locations = ["Friedrichstraße 180, Berlin", "Brandenburger Tor, Berlin"]  # Example list of addresses
+
+points = []
+
+for address in locations:
+    coord = geocoder.geocode_address(address)
+    if coord:  # Check that result was returned
+        points.append([coord[k] for k in ["lat", "lng"]])
+    
+    centroid = MultiPoint(points).centroid
+
+if location:
+    print("Latitude:", location["lat"])
+    print("Longitude:", location["lng"])
 
 # Request headers
 headers = {
@@ -22,8 +47,8 @@ payload = {
     "locationRestriction": {
         "circle": {
             "center": {
-                "latitude": 37.7937,
-                "longitude": -122.3965
+                "latitude": centroid["lat"],
+                "longitude": centroid["lng"]
             },
             "radius": 500.0
         }
@@ -37,7 +62,9 @@ response = requests.post(url, headers=headers, data=json.dumps(payload))
 if response.status_code == 200:
     print("Success! Response data:")
     print(json.dumps(response.json(), indent=2))
-    pd.json_normalize(response.json(), record_path=["places"]).to_csv("toydata.csv", index=False)
 else:
     print(f"Error {response.status_code}: {response.text}")
 
+# Here goes the filtering pipeline, e.g. for card only restaurants or vegetarian options
+
+# Here goes the calculation of distance to centroid
